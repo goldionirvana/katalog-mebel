33b1d40958635c774d35f0ee3d041a9abbcb6d4e
fazarashif <55146306+fazarashif@users.noreply.github.com>
2020-04-23 20:48:08 +0700

    update index.php dikit
    banyak eh yang diubah

--- bintangterang/index.php
+++ bintangterang/index.php
@@ -9,8 +9,17 @@ if( !isset($_SESSION["login"]) ) {
 
 require 'functions.php';
 
+// pagination
+// konfigurasi
+$jumlahDataPerHalaman = 4;
+$jumlahData = count(query("SELECT * FROM produk"));
+$jumlahHalaman = ceil($jumlahData / $jumlahDataPerHalaman);
+$halamanAktif = ( isset($_GET["halaman"]) ) ? $_GET["halaman"] : 1;
+$awalData = ( $jumlahDataPerHalaman * $halamanAktif ) - $jumlahDataPerHalaman;
+
+
 //query data yang mau ditambpilkan
-$mebel = query("SELECT * FROM produk");
+$mebel = query("SELECT * FROM produk LIMIT $awalData, $jumlahDataPerHalaman");
 
 //jika tombol cari diklik
 if( isset($_POST["cari"]) ) {
@@ -40,7 +49,30 @@ if( isset($_POST["cari"]) ) {
         <input type="text" name="keyword" size="40" autofocus placeholder="masukkan keyword pencarian.." autocomplete="off" id="keyword">
         <button type="submit" name="cari">SEARCH</button>
     </form>
-    
+
+
+	<!-- navigasi halaman -->
+<a href="?halaman=1">awal</a>
+
+<?php if( $halamanAktif > 1 ) : ?>
+	<a href="?halaman=<?= $halamanAktif - 1; ?>">&laquo;</a>
+<?php endif; ?>
+
+<?php for( $i = 1; $i <= $jumlahHalaman; $i++ ) : ?>
+	<?php if( $i == $halamanAktif ) : ?>
+		<a href="?halaman=<?= $i; ?>" style="font-weight: bold; color: red;"><?= $i; ?></a>
+	<?php else : ?>
+		<a href="?halaman=<?= $i; ?>"><?= $i; ?></a>
+	<?php endif; ?>
+<?php endfor; ?>
+
+<?php if( $halamanAktif < $jumlahHalaman ) : ?>
+	<a href="?halaman=<?= $halamanAktif + 1; ?>">&raquo;</a>
+<?php endif; ?>
+
+<a href="?halaman=<?= $jumlahHalaman; ?>">akhir</a>
+
+
     <br>
     <table border="1" cellpadding="10" cellspacing="0">
     
@@ -66,7 +98,7 @@ if( isset($_POST["cari"]) ) {
 			<a href="ubah.php?id_produk=<?= $row["id_produk"]; ?>">update</a> |
 			<a href="hapus.php?id_produk=<?= $row["id_produk"]; ?>" onclick="return confirm('data akan dihapus. Lanjutkan?');">hapus</a>
 		</td>
-		<td><img src="img/<?= $row["gambar"]; ?>" width="200"></td>
+		<td><img src="img/<?= $row["gambar"]; ?>" width="150"></td> 
 		<td><?= $row["nama"]; ?></td>
 		<td><?= $row["deskripsi"]; ?></td>
 		<td><?= $row["harga"]; ?></td>
