bb7d225f38ef02cfe760b7b86eee533875829bc9
fazarashif <55146306+fazarashif@users.noreply.github.com>
2020-05-13 12:38:21 +0700

    Add files via upload

---
+++ toko_online/masuk_admin/footer.php
@@ -0,0 +1,12 @@
+
+
+<div id="footer">
+<div id="find">
+<center>
+<small> Copyright &#169;<script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script>&nbsp;
+Design & Maintenance by &nbsp;<a href='#'>Kelompok Mebel</a></small></center>
+</div>
+</div>
+
+</div>
+</div>
